929ce09f02ef0c0558259660a6364f59631a2986
morgenz <pawel.stanislaw.kmita@gmail.com>
2020-02-28 22:15:41 +0100

    TicTacToe hyperskill code refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,53 +4,53 @@ import java.util.Random;
 import java.util.Scanner;
 
 interface Player {
-    // Marcin: Methods in interfaces are public by default
-    public void makeMove(char[][] board);
+    void makeMove(char[][] board);
 }
 
-// Marcin: Classes should start with capital letter
-class humanPlayer implements Player {
+
+class HumanPlayer implements Player {
     char xOrO;
 
-    humanPlayer(char xOrO) {
+    HumanPlayer(char xOrO) {
         this.xOrO = xOrO;
     }
 
     public void makeMove(char[][] board) {
         Scanner scanner = new Scanner(System.in);
-
         // Marcin: Formatting
-        while(true){
+        while (true) {
             System.out.println("Enter the coordinates: ");
-        try {
-            // Marcin: Integer.parseInt logic should be extracted to variable
-            enterCoords(board, Integer.parseInt(scanner.next()), Integer.parseInt(scanner.next()), xOrO);
-            return;
-        } catch (Exception e) {
-            if (e instanceof NumberFormatException) // Marcin: Brackets
-                System.out.println("You should enter numbers!");
-            else // Marcin: Brackets
-                System.out.println(e.getMessage());
-        }
+            try {
+                enterCoords(board, Integer.parseInt(scanner.next()), Integer.parseInt(scanner.next()), xOrO);
+                return;
+            } catch (Exception e) {
+                if (e instanceof NumberFormatException) {
+                    System.out.println("You should enter numbers!");
+                } else {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
     }
 
     void enterCoords(char[][] arr, int x, int y, char s) {
         x--;
         y--;
-        if (x < 0 || x > 2 || y < 0 || y > 2) // Marcin: Brackets
+        if (x < 0 || x > 2 || y < 0 || y > 2) {
             throw new IllegalArgumentException("Coordinates should be from 1 to 3!");
-        if (arr[2 - y][x] != '_') // Marcin: Brackets
+        }
+        if (arr[2 - y][x] != '_') {
             throw new IllegalArgumentException("This cell is occupied! Choose another one!");
+        }
         arr[2 - y][x] = s;
     }
 }
 
-// Classes should start with capital letter
-class easyComputerPlayer implements Player {
+
+class EasyComputerPlayer implements Player {
     char xOrO;
 
-    easyComputerPlayer(char xOrO) {
+    EasyComputerPlayer(char xOrO) {
         this.xOrO = xOrO;
     }
 
@@ -67,11 +67,11 @@ class easyComputerPlayer implements Player {
     }
 }
 
-// Classes should start with capital letter
-class mediumComputerPlayer implements Player {
+
+class MediumComputerPlayer implements Player {
     char xOrO;
 
-    mediumComputerPlayer(char xOrO) {
+    MediumComputerPlayer(char xOrO) {
         this.xOrO = xOrO;
     }
 
@@ -80,8 +80,7 @@ class mediumComputerPlayer implements Player {
         System.out.println("Making move level \"medium\"");
         int x = Math.abs(generator.nextInt() % 3);
         int y = Math.abs(generator.nextInt() % 3);
-        // formatting
-        if(tryToWin(board)||tryNotToLose(board)){ // Marcin: Formatting
+        if (tryToWin(board) || tryNotToLose(board)) {
             return;
         }
         while (board[x][y] != '_') {
@@ -97,76 +96,83 @@ class mediumComputerPlayer implements Player {
         // Code is duplicated with tryToLose function
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (board[i][j] == xOrO) // Marcin: Brackets
+                if (board[i][j] == xOrO) {
                     counter++;
-                else if (!(board[i][j] == '_')) {
+                } else if (!(board[i][j] == '_')) {
                     counter--;
                 }
             }
             if (counter == 2) {
                 for (int j = 0; j < 3; j++)
-                    if (board[i][j] == '_') // Marcin: Brackets
+                    if (board[i][j] == '_') {
                         board[i][j] = xOrO;
-               return true;
+                    }
+                return true;
             }
-            counter=0;
+            counter = 0;
 
             for (int j = 0; j < 3; j++) {
-                if (board[j][i] == xOrO) // Marcin: Brackets
+                if (board[j][i] == xOrO) {
                     counter++;
-                else if (!(board[j][i] == '_')) {
+                } else if (!(board[j][i] == '_')) {
                     counter--;
                 }
             }
             if (counter == 2) {
-                for (int j = 0; j < 3; j++) // Marcin: Brackets
-                    if (board[j][i] == '_') // Marcin: Brackets
+                for (int j = 0; j < 3; j++) {
+                    if (board[j][i] == '_') {
                         board[j][i] = xOrO;
+                    }
+                }
                 return true;
             }
-            counter=0;
+            counter = 0;
 
         }
-    return false;
+        return false;
     }
+
     public boolean tryNotToLose(char[][] board) {
         int counter = 0;
         char enemy;
-        if(xOrO=='X')  // Marcin: Brackets
-            enemy='O';
-        else // Marcin: Brackets
-            enemy='X';
+        if (xOrO == 'X') {
+            enemy = 'O';
+        } else {
+            enemy = 'X';
+        }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (board[i][j] == enemy) // Marcin: Brackets
+                if (board[i][j] == enemy) {
                     counter++;
-                else if (!(board[i][j] == '_')) {
+                } else if (!(board[i][j] == '_')) {
                     counter--;
                 }
             }
             if (counter == 2) {
                 for (int j = 0; j < 3; j++)
-                    if (board[i][j] == '_') // Marcin: Brackets
+                    if (board[i][j] == '_') {
                         board[i][j] = xOrO;
+                    }
                 return true;
             }
-            counter=0;
+            counter = 0;
 
             for (int j = 0; j < 3; j++) {
-                if (board[j][i] == enemy) // Marcin: Brackets
+                if (board[j][i] == enemy) {
                     counter++;
-                else if (!(board[j][i] == '_')) {
+                } else if (!(board[j][i] == '_')) {
                     counter--;
                 }
             }
             if (counter == 2) {
-                for (int j = 0; j < 3; j++)  // Marcin: Brackets
-                    if (board[j][i] == '_') // Marcin: Brackets
+                for (int j = 0; j < 3; j++) {
+                    if (board[j][i] == '_') {
                         board[j][i] = xOrO;
+                    }
+                }
                 return true;
             }
-            counter=0;
-
+            counter = 0;
         }
         return false;
     }
@@ -174,56 +180,56 @@ class mediumComputerPlayer implements Player {
 
 class Game {
     // Marcin: That string array could be replaced to enum
-    static String[] typeOfPlayers = {"easy", "user","medium"};
-    char[][] board;
-    // Marcin: Mode static variable can be replaced with throwing exception and handling it in main function :)
-    static String mode = "0";
-    Player player1;
-    Player player2;
-
-    public static String getMode() {
-        return mode;
+    enum PlayerType {
+        easy, user, medium;
     }
+    char[][] board;
+    Player[] players;
+
 
     public Game() {
-        // Marcin: Constructor should not have any logic
         this.board = setEmptyBoard(new char[3][3]);
+        players = setPlayers();
+    }
+
+    Player[] setPlayers() {
         String[] userInput = getUserInput();
-        if (!userInput[0] .equals( "exit")) {
-            mode = userInput[0];
-            // Marcin: This logic can be extracted to method
+        Player[] players = new Player[2];
+        if (userInput[0].equals("exit")) {
+            throw new RuntimeException();
+        } else {
+
             switch (userInput[1]) {
                 case "user":
-                    player1 = new humanPlayer('X');
+                    players[0] = new HumanPlayer('X');
                     break;
 
                 case "easy":
-                    player1 = new easyComputerPlayer('X');
+                    players[0] = new EasyComputerPlayer('X');
                     break;
                 case "medium":
-                    player1 = new mediumComputerPlayer('X');
+                    players[0] = new MediumComputerPlayer('X');
                     break;
 
             }
             switch (userInput[2]) {
                 case "user":
-                    player2 = new humanPlayer('O');
+                    players[1] = new HumanPlayer('O');
                     break;
                 case "easy":
-                    player2 = new easyComputerPlayer('O');
+                    players[1] = new EasyComputerPlayer('O');
                     break;
                 case "medium":
-                    player2 = new mediumComputerPlayer('O');
+                    players[1] = new MediumComputerPlayer('O');
                     break;
             }
-        } else {
-            mode = "exit";
         }
+        return players;
     }
 
     static char[][] setEmptyBoard(char[][] arr) {
-        // Marcin: Number '3' can be extracted to variable. It is a magic number right now
-        for (int i = 0; i < 3; i++) {
+        int numberOfRows = 3;
+        for (int i = 0; i < numberOfRows; i++) {
             Arrays.fill(arr[i], '_');
         }
         return arr;
@@ -233,11 +239,13 @@ class Game {
         System.out.println("---------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
-            for (int j = 0; j < 3; j++)  // Marcin: Brackets
-                if (this.board[i][j] == '_') // Marcin: Brackets
+            for (int j = 0; j < 3; j++) {
+                if (this.board[i][j] == '_') {
                     System.out.print("  ");
-                else // Marcin: Brackets
+                } else {
                     System.out.print(this.board[i][j] + " ");
+                }
+            }
             System.out.println("|");
         }
         System.out.println("---------");
@@ -246,10 +254,8 @@ class Game {
     static char getWinner(char[][] arr) {
 
         for (int i = 0; i < 3; i++) {
-            //poziomo // Marcin: Is that comment necessary? It can be moved to method :)
             if (arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2] && arr[i][0] != '_')
                 return arr[i][0];
-            //pionowo // Marcin: Is that comment necessary? It can be moved to method :)
             if (arr[0][i] == arr[1][i] && arr[0][i] == arr[2][i] && arr[0][i] != '_')
                 return arr[0][i];
         }
@@ -258,28 +264,29 @@ class Game {
 
         if (arr[2][0] == arr[1][1] && arr[1][1] == arr[0][2] && arr[0][2] != '_')
             return arr[0][2];
-        for (int i = 0; i < 3; i++)  // Marcin: Brackets
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (arr[i][j] == '_')
                     return '_';
             }
+        }
         return 'D';
     }
 
     void playTheGame() {
-        if (mode.equals("exit")) {
-            return;
-        }
+
         printGame();
         while (getWinner(board) == '_') {
-            player1.makeMove(board);
+            players[0].makeMove(board);
             printGame();
-            if (getWinner(board) != '_') // Marcin: Brackets
+            if (getWinner(board) != '_') {
                 break;
-            player2.makeMove(board);
+            }
+            players[1].makeMove(board);
             printGame();
-            if (getWinner(board) != '_') // Marcin: Brackets
+            if (getWinner(board) != '_') {
                 break;
+            }
         }
         printWinner(board);
     }
@@ -290,32 +297,49 @@ class Game {
         String[] modeAndPlayers = scanner.nextLine().split(" ");
         while (!modeAndPlayers[0].equals("start")
                 || modeAndPlayers.length != 3
-                || !Arrays.asList(typeOfPlayers).containsAll(Arrays.asList(modeAndPlayers[1], modeAndPlayers[2]))) {
-            if (modeAndPlayers[0].equals("exit")) // Marcin: Brackets
+                || checkIfPlayerNameIsInvalid(modeAndPlayers[1])
+                || checkIfPlayerNameIsInvalid(modeAndPlayers[2])) {
+            if (modeAndPlayers[0].equals("exit")) {
                 return new String[]{"exit"};
+            }
             System.out.println("Bad parameters!");
             modeAndPlayers = scanner.nextLine().split(" ");
         }
         return modeAndPlayers;
     }
 
+    boolean checkIfPlayerNameIsInvalid(String player) {
+        for (PlayerType p : PlayerType.values()) {
+            if (p.name().equals(player)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     static void printWinner(char[][] board) {
         if (getWinner(board) == 'X') {
             System.out.println("X wins");
-        } else if (getWinner(board) == 'O')  // Marcin: Brackets
+        } else if (getWinner(board) == 'O') {
             System.out.println("O wins");
-        else if (getWinner(board) == 'D')  // Marcin: Brackets
+        } else if (getWinner(board) == 'D') {
             System.out.println("Draw");
-        else  // Marcin: Brackets
+        } else {
             System.out.println("Game not finished");
+        }
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        while (!Game.getMode().equals("exit")) {
-            Game game = new Game();
-            game.playTheGame();
+        while (true) {
+            try {
+                Game game = new Game();
+                game.playTheGame();
+            } catch (RuntimeException e) {
+                break;
+            }
+
         }
     }
 }
